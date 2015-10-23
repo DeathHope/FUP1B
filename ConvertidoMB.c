f78b8085ffc7f153097bb35bac48e9ed20f65918
Gerardo Hernandez <geheter@hotmail.com>
2015-10-22 22:43:05 -0500

    Tienda2.0Dfd

--- ConvertidoMB.c
+++ ConvertidoMB.c
@@ -2,6 +2,7 @@
 /*
 calculador de MB
 */
+
 int resultado1;
 int resultado2;
 int resultado3;
@@ -9,6 +10,7 @@ float resultado4;
 double resultado5;
 int N1; 
 int main()
+
 {//inicio
 printf("Esbribe la cantidad total:");
 scanf("%d", &N1); 
@@ -17,10 +19,10 @@ resultado2= N1*1048576;
 resultado3= N1*1024;
 resultado4= N1/1024;
 resultado5= resultado4/1024;
-printf("\n la conversion a bits es de:%d\n", resultado1);
-printf("\n la conversion a bytes es de:%d\n", resultado2);
-printf("\n la conversion a kilobytes es de:%d\n", resultado3);
-printf("\n la conversion a gigabytes es de:%f\n", resultado4);
-printf("\n la conversion a terabytes es de:%f\n", resultado5);
+printf("\n La conversion a bits es de:%d\n", resultado1);
+printf("\n La conversion a bytes es de:%d\n", resultado2);
+printf("\n La conversion a kilobytes es de:%d\n", resultado3);
+printf("\n La conversion a gigabytes es de:%f\n", resultado4);
+printf("\n La conversion a terabytes es de:%f\n", resultado5);
 system("pause");
 }//fin
